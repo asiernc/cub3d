177730456d524cf0378bda7dfc49e36e943e8a44
molasz <martiolasz@gmail.com>
2024-07-20 17:09:00 +0200

    add movement margins

--- src/controls/move.c
+++ src/controls/move.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz-a@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:32:35 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/07/20 14:14:24 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/07/20 17:05:14 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@ void	move_front(t_player_mlx *player, char **map)
 {
 	int	x;
 	int	y;
+	int	nx;
+	int	ny;
 
-	x = (player->pos.x + player->dir.x * MOVE_SPEED) / TILE_SIZE;
-	y = (player->pos.y + player->dir.y * MOVE_SPEED) / TILE_SIZE;
-	
-	if (map[player->pos.y / TILE_SIZE][x] == '0')
+	x = (player->pos.x + player->dir.x * MOVE_SPEED) / TILE_SIZE + 0.2;
+	y = (player->pos.y + player->dir.y * MOVE_SPEED) / TILE_SIZE + 0.2;
+	nx = (player->pos.x + player->dir.x * MOVE_SPEED) / TILE_SIZE - 0.2;
+	ny = (player->pos.y + player->dir.y * MOVE_SPEED) / TILE_SIZE - 0.2;
+	if (map[player->pos.y / TILE_SIZE][x] == '0'
+			&& map[player->pos.y / TILE_SIZE][nx] == '0')
 		player->pos.x += player->dir.x * MOVE_SPEED;
-	if (map[y][player->pos.x / TILE_SIZE] == '0')
+	if (map[y][player->pos.x / TILE_SIZE] == '0'
+			&& map[ny][player->pos.x / TILE_SIZE] == '0')
 		player->pos.y += player->dir.y * MOVE_SPEED;
 }
 
@@ -30,12 +35,18 @@ void	move_back(t_player_mlx *player, char **map)
 {
 	int	x;
 	int	y;
+	int	nx;
+	int	ny;
 
-	x = (player->pos.x - player->dir.x * MOVE_SPEED) / TILE_SIZE;
-	y = (player->pos.y - player->dir.y * MOVE_SPEED) / TILE_SIZE;
-	if (map[player->pos.y / TILE_SIZE][x] == '0')
+	x = (player->pos.x - player->dir.x * MOVE_SPEED) / TILE_SIZE + 0.2;
+	y = (player->pos.y - player->dir.y * MOVE_SPEED) / TILE_SIZE + 0.2;
+	nx = (player->pos.x - player->dir.x * MOVE_SPEED) / TILE_SIZE - 0.2;
+	ny = (player->pos.y - player->dir.y * MOVE_SPEED) / TILE_SIZE - 0.2;
+	if (map[player->pos.y / TILE_SIZE][x] == '0'
+			&& map[player->pos.y / TILE_SIZE][nx] == '0')
 		player->pos.x -= player->dir.x * MOVE_SPEED;
-	if (map[y][player->pos.x / TILE_SIZE] == '0')
+	if (map[y][player->pos.x / TILE_SIZE] == '0'
+			&& map[ny][player->pos.x / TILE_SIZE] == '0')
 		player->pos.y -= player->dir.y * MOVE_SPEED;
 }
 
@@ -43,12 +54,18 @@ void	move_right(t_player_mlx *player, char **map)
 {
 	int	x;
 	int	y;
+	int	nx;
+	int	ny;
 
-	x = (player->pos.x - player->dir.y * MOVE_SPEED) / TILE_SIZE;
-	y = (player->pos.y + player->dir.x * MOVE_SPEED) / TILE_SIZE;
-	if (map[player->pos.y / TILE_SIZE][x] == '0')
+	x = (player->pos.x - player->dir.y * MOVE_SPEED) / TILE_SIZE + 0.2;
+	y = (player->pos.y + player->dir.x * MOVE_SPEED) / TILE_SIZE + 0.2;
+	nx = (player->pos.x - player->dir.y * MOVE_SPEED) / TILE_SIZE - 0.2;
+	ny = (player->pos.y + player->dir.x * MOVE_SPEED) / TILE_SIZE - 0.2;
+	if (map[player->pos.y / TILE_SIZE][x] == '0'
+			&& map[player->pos.y / TILE_SIZE][nx] == '0')
 		player->pos.x -= player->dir.y * MOVE_SPEED;
-	if (map[y][player->pos.x / TILE_SIZE] == '0')
+	if (map[y][player->pos.x / TILE_SIZE] == '0'
+			&& map[ny][player->pos.x / TILE_SIZE] == '0')
 		player->pos.y += player->dir.x * MOVE_SPEED;
 }
 
@@ -56,12 +73,18 @@ void	move_left(t_player_mlx *player, char **map)
 {
 	int	x;
 	int	y;
+	int	nx;
+	int	ny;
 
-	x = (player->pos.x + player->dir.y * MOVE_SPEED) / TILE_SIZE;
-	y = (player->pos.y - player->dir.x * MOVE_SPEED) / TILE_SIZE;
-	if (map[player->pos.y / TILE_SIZE][x] == '0')
+	x = (player->pos.x + player->dir.y * MOVE_SPEED) / TILE_SIZE + 0.2;
+	y = (player->pos.y - player->dir.x * MOVE_SPEED) / TILE_SIZE + 0.2;
+	nx = (player->pos.x + player->dir.y * MOVE_SPEED) / TILE_SIZE - 0.2;
+	ny = (player->pos.y - player->dir.x * MOVE_SPEED) / TILE_SIZE - 0.2;
+	if (map[player->pos.y / TILE_SIZE][x] == '0'
+			&& map[player->pos.y / TILE_SIZE][nx] == '0')
 		player->pos.x += player->dir.y * MOVE_SPEED;
-	if (map[y][player->pos.x / TILE_SIZE] == '0')
+	if (map[y][player->pos.x / TILE_SIZE] == '0'
+			&& map[ny][player->pos.x / TILE_SIZE] == '0')
 		player->pos.y -= player->dir.x * MOVE_SPEED;
 }
 
