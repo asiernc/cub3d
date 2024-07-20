8b7672ec58bfbaedd02963c4e48b5ca46c41c73e
molasz <martiolasz@gmail.com>
2024-07-20 13:27:59 +0200

    fix left right movement

--- src/controls/move.c
+++ src/controls/move.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz-a@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:32:35 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/07/18 21:26:09 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/07/20 13:26:44 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ void	move_right(t_player_mlx *player, char **map)
 
 	x = (player->pos.x - player->dir.y * MOVE_SPEED) / 200;
 	y = (player->pos.y - player->dir.x * MOVE_SPEED) / 200;
-	if (map[player->pos.y / 200][x] == '0')
-		player->pos.x -= player->dir.y * MOVE_SPEED;
 	if (map[y][player->pos.x / 200] == '0')
-		player->pos.y -= player->dir.x * MOVE_SPEED;
+		player->pos.x -= player->dir.y * MOVE_SPEED;
+	if (map[player->pos.y / 200][x] == '0')
+		player->pos.y += player->dir.x * MOVE_SPEED;
 }
 
 void	move_left(t_player_mlx *player, char **map)
@@ -61,7 +61,7 @@ void	move_left(t_player_mlx *player, char **map)
 	if (map[player->pos.y / 200][x] == '0')
 		player->pos.x += player->dir.y * MOVE_SPEED;
 	if (map[y][player->pos.x / 200] == '0')
-		player->pos.y += player->dir.x * MOVE_SPEED;
+		player->pos.y -= player->dir.x * MOVE_SPEED;
 }
 
 void	move_action(t_cub3d *cub3d)
